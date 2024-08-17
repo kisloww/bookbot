33a9621e5bd506a12753fc7f03cbc8f5e1b3c8c7
Степан <stepashka@192.168.1.19>
2024-08-17 17:41:13 +0300

    define count_characters

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ def main():
     path = "books/frankenstein.txt"
     with open(path) as f:
         file_content = f.read()
-    print(count_words(file_content))
-
+    # print(count_words(file_content))
+    result = count_characters(file_content)
+    print(result)
 
 def count_words(text):
     words = text.split()
     return len(words)
 
-
-
+def count_characters(text):
+    lower_text = text.lower()
+    characters = {}
+    for character in lower_text:
+        if character not in characters:
+             characters[character] = 1
+        else:
+            characters[character] += 1
+    return characters
 
 
 main()
